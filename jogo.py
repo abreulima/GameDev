ca6c82f023144582dddfe0c11d70f064a2c5f18e
Ivan <34632454+abreulima@users.noreply.github.com>
2026-06-16 11:11:03 +0100

    Vida, e fundo adicionados

--- jogo.py
+++ jogo.py
@@ -17,9 +17,13 @@ OURO = (255, 215, 0)
 
 jogador_img = pygame.image.load("artes/jogador.png").convert_alpha()
 inimigo_img = pygame.image.load("artes/inimigo.png").convert_alpha()
+fundo_img = pygame.image.load("artes/fundo.png").convert()
+vida_img = pygame.image.load("artes/vida.png").convert_alpha()
 
 jogador_img = pygame.transform.scale(jogador_img, (50, 60))
 inimigo_img = pygame.transform.scale(inimigo_img, (50, 50))
+fundo_img = pygame.transform.scale(fundo_img, (LARGURA, ALTURA))
+vida_img = pygame.transform.scale(vida_img, (32, 32))
 
 jogador = pygame.Rect(100, 300, 50, 60)
 vel_y = 0
@@ -27,6 +31,8 @@ velocidade = 5
 gravidade = 0.8
 forca_pulo = -15
 no_chao = False
+vidas = 3
+jogo_terminou = False
 
 LARGURA_NIVEL = 2600
 
@@ -57,9 +63,32 @@ objetivo = pygame.Rect(2500, 190, 50, 70)
 
 fonte = pygame.font.SysFont(None, 40)
 
+def desenhar_fundo(camera_x):
+    deslocamento = int(camera_x * 0.35) % LARGURA
+
+    tela.blit(fundo_img, (-deslocamento, 0))
+    tela.blit(fundo_img, (LARGURA - deslocamento, 0))
+
 def mostrar_texto(texto):
     img = fonte.render(texto, True, PRETO)
     tela.blit(img, (LARGURA // 2 - img.get_width() // 2, 40))
+
+def mostrar_vidas():
+    for i in range(vidas):
+        tela.blit(vida_img, (20 + i * 38, 20))
+
+def reiniciar_jogador():
+    global vel_y, no_chao
+
+    jogador.x, jogador.y = 100, 300
+    vel_y = 0
+    no_chao = False
+
+def perder_vida():
+    global vidas
+
+    vidas -= 1
+    reiniciar_jogador()
 
 while True:
     relogio.tick(60)
@@ -69,60 +98,64 @@ while True:
             pygame.quit()
             sys.exit()
 
-    teclas = pygame.key.get_pressed()
+    venceu = jogador.colliderect(objetivo)
+    jogo_terminou = venceu or vidas <= 0
 
-    dx = 0
-    if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
-        dx = -velocidade
-    if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
-        dx = velocidade
+    if not jogo_terminou:
+        teclas = pygame.key.get_pressed()
 
-    if (teclas[pygame.K_SPACE] or teclas[pygame.K_UP]) and no_chao:
-        vel_y = forca_pulo
+        dx = 0
+        if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
+            dx = -velocidade
+        if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
+            dx = velocidade
+
+        if (teclas[pygame.K_SPACE] or teclas[pygame.K_UP]) and no_chao:
+            vel_y = forca_pulo
+            no_chao = False
+
+        jogador.x += dx
+
+        if jogador.left < 0:
+            jogador.left = 0
+        if jogador.right > LARGURA_NIVEL:
+            jogador.right = LARGURA_NIVEL
+
+        vel_y += gravidade
+        jogador.y += vel_y
+
         no_chao = False
 
-    jogador.x += dx
+        for plataforma in plataformas:
+            if jogador.colliderect(plataforma):
+                if vel_y > 0:
+                    jogador.bottom = plataforma.top
+                    vel_y = 0
+                    no_chao = True
+                elif vel_y < 0:
+                    jogador.top = plataforma.bottom
+                    vel_y = 0
 
-    if jogador.left < 0:
-        jogador.left = 0
-    if jogador.right > LARGURA_NIVEL:
-        jogador.right = LARGURA_NIVEL
+        for inimigo in inimigos:
+            inimigo["rect"].x += inimigo["vel"]
 
-    vel_y += gravidade
-    jogador.y += vel_y
+            if inimigo["rect"].left < inimigo["min"] or inimigo["rect"].right > inimigo["max"]:
+                inimigo["vel"] *= -1
 
-    no_chao = False
+            if jogador.colliderect(inimigo["rect"]):
+                perder_vida()
+                break
 
-    for plataforma in plataformas:
-        if jogador.colliderect(plataforma):
-            if vel_y > 0:
-                jogador.bottom = plataforma.top
-                vel_y = 0
-                no_chao = True
-            elif vel_y < 0:
-                jogador.top = plataforma.bottom
-                vel_y = 0
+        if jogador.top > ALTURA:
+            perder_vida()
 
-    for inimigo in inimigos:
-        inimigo["rect"].x += inimigo["vel"]
-
-        if inimigo["rect"].left < inimigo["min"] or inimigo["rect"].right > inimigo["max"]:
-            inimigo["vel"] *= -1
-
-        if jogador.colliderect(inimigo["rect"]):
-            jogador.x, jogador.y = 100, 300
-            vel_y = 0
-
-    if jogador.top > ALTURA:
-        jogador.x, jogador.y = 100, 300
-        vel_y = 0
-
-    venceu = jogador.colliderect(objetivo)
+        venceu = jogador.colliderect(objetivo)
+        jogo_terminou = venceu or vidas <= 0
 
     camera_x = jogador.centerx - LARGURA // 2
     camera_x = max(0, min(camera_x, LARGURA_NIVEL - LARGURA))
 
-    tela.fill(AZUL_CEU)
+    desenhar_fundo(camera_x)
 
     for plataforma in plataformas:
         p = plataforma.move(-camera_x, 0)
@@ -136,8 +169,11 @@ while True:
         tela.blit(inimigo_img, inimigo["rect"].move(-camera_x, 0))
 
     tela.blit(jogador_img, jogador.move(-camera_x, 0))
+    mostrar_vidas()
 
     if venceu:
         mostrar_texto("Você venceu!")
+    elif vidas <= 0:
+        mostrar_texto("Fim de jogo!")
 
     pygame.display.flip()
